0e634d17d0f62a030fb2dc82c4049ddf626cb251
Ansar Ismagilov <119264390+kabgig@users.noreply.github.com>
2024-08-24 17:18:32 +0600

    fixed font

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,13 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import NavBar from "./NavBar";
-import { Theme, ThemePanel } from "@radix-ui/themes";
+import { Theme } from "@radix-ui/themes";
 
-const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
