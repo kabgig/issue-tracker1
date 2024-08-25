1f7da838279ea646d69e4661c80e2d717f20bc8b
Ansar Ismagilov <119264390+kabgig@users.noreply.github.com>
2024-08-25 16:41:53 +0600

    Disables SSR for md editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -3,14 +3,18 @@ import ErrorMessage from "@/app/components/ErrorMessage";
 import Spinner from "@/app/components/Spinner";
 import { createIssueSchema } from "@/app/validationSchemas";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Button, Callout, Text, TextField } from "@radix-ui/themes";
+import { Button, Callout, TextField } from "@radix-ui/themes";
 import axios from "axios";
 import "easymde/dist/easymde.min.css";
+import dynamic from "next/dynamic";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
-import { Controller, set, useForm } from "react-hook-form";
-import SimpleMDE from "react-simplemde-editor";
+import { Controller, useForm } from "react-hook-form";
 import { z } from "zod";
+
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
