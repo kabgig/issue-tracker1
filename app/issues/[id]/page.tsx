2316467d8377b8a670b10f8acd40b5f82babf5bd
Ansar Ismagilov <119264390+kabgig@users.noreply.github.com>
2024-08-31 18:03:36 +0600

    building select assignee component 2

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,13 +1,12 @@
-"use client";
+import authOptions from "@/app/auth/AuthOptions";
 import prisma from "@/prisma/client";
 import { Box, Flex, Grid } from "@radix-ui/themes";
+import { getServerSession } from "next-auth";
 import { notFound } from "next/navigation";
+import AssigneeSelect from "./AssigneeSelect";
+import DeleteIssueButton from "./DeleteIssueButton";
 import EditIssueButton from "./EditIssueButton";
 import IssueDetails from "./IssueDetails";
-import DeleteIssueButton from "./DeleteIssueButton";
-import { getServerSession } from "next-auth";
-import authOptions from "@/app/auth/AuthOptions";
-import AssigneeSelect from "./AssigneeSelect";
 
 interface Props {
   params: { id: string };
@@ -30,6 +29,7 @@ const IssueDetailPage = async ({ params }: Props) => {
       <Box className="md:col-span-4">
         <IssueDetails issue={issue} />
       </Box>
+
       {session && (
         <Box>
           <Flex direction="column" gap="4">
